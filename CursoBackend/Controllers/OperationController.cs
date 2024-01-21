c30808eeaee9148c9a298de2a8a0912f0b2997a3
Marco Antonio Carrillo Esparza <marco.37099p@gmail.com>
2024-01-21 17:09:27 -0600

    Agregar archivos de proyecto.

---
+++ CursoBackend/Controllers/OperationController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CursoBackend.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class OperationController : ControllerBase
+    {
+        [HttpGet]
+        public decimal Get(decimal a, decimal b)
+        {
+            return a + b;
+        }
+
+        [HttpPost]
+        public decimal Add(decimal a, decimal b)
+        {
+            return a - b;
+        }
+
+        [HttpPut]
+        public decimal Edit(decimal a, decimal b)
+        {
+            return a * b;
+        }
+
+        [HttpDelete]
+        public decimal Delete(decimal a, decimal b)
+        {
+            return a / b;
+        }
+    }
+}
