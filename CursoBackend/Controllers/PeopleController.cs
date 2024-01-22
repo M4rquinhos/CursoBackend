f1b690688e0fe31097fcb40cfd361ef55c8a3dc8
Marco Antonio Carrillo Esparza <marco.37099p@gmail.com>
2024-01-21 21:00:30 -0600

    Respuestas HTTP. ActionResult y IActionResult

--- CursoBackend/Controllers/PeopleController.cs
+++ CursoBackend/Controllers/PeopleController.cs
@@ -9,6 +9,34 @@ namespace CursoBackend.Controllers
     {
         [HttpGet("all")]
         public List<People> GetPeople() => Repository.People;
+
+        [HttpGet("{id}")]
+        public ActionResult<People> Get(int id)
+        {
+            var people = Repository.People.FirstOrDefault(p => p.Id == id);
+
+            if (people == null)
+            {
+                return NotFound();
+            }
+            return Ok(people);
+        }
+
+        [HttpGet("search/{search}")]
+        public List<People> Get(string search) =>
+            Repository.People.Where(p => p.Name.ToUpper().Contains(search.ToUpper())).ToList();
+
+        [HttpPost]
+        public IActionResult Add(People people)
+        {
+            if (string.IsNullOrEmpty(people.Name))
+            {
+                return BadRequest();
+            }
+
+            Repository.People.Add(people);
+            return NoContent();
+        }
     }
 
     public class Repository
