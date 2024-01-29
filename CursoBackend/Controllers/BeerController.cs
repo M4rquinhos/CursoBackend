a46a8971a43e67ff53105c625d5f6506493cf26d
Marco Antonio Carrillo Esparza <marco.37099p@gmail.com>
2024-01-28 22:41:39 -0600

    CRUD entity framework

--- CursoBackend/Controllers/BeerController.cs
+++ CursoBackend/Controllers/BeerController.cs
@@ -46,5 +46,69 @@ namespace CursoBackend.Controllers
 
             return Ok(beerDto);
         }
+
+        [HttpPost]
+        public async Task<ActionResult<BeerDto>> Add(BeerInsertDto beerInsertDto)
+        {
+            var beer = new Beer()
+            {
+                Name = beerInsertDto.Name,
+                BrandId = beerInsertDto.BrandId,
+                Alcohol = beerInsertDto.Alcohol
+            };
+
+            await _context.Beers.AddAsync(beer);
+            await _context.SaveChangesAsync();
+
+            var beerDto = new BeerDto()
+            {
+                Id = beer.BeerId,
+                Name = beer.Name,
+                Alcohol = beer.Alcohol,
+                BrandId = beer.BrandId
+            };
+
+            return CreatedAtAction(nameof(GetById), new { id = beer.BeerId }, beerDto);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<ActionResult<BeerDto>> Update(int id, BeerUpdateDto beerUpdateDto)
+        {
+            var beer = await _context.Beers.FindAsync(id);
+            if (beer == null)
+            {
+                return NotFound();
+            }
+
+            beer.Name = beerUpdateDto.Name;
+            beer.Alcohol = beerUpdateDto.Alcohol;
+            beer.BrandId = beerUpdateDto.BrandId;
+            await _context.SaveChangesAsync();
+
+            var beerDto = new BeerDto()
+            {
+                Id = beer.BeerId,
+                Name = beer.Name,
+                Alcohol = beer.Alcohol,
+                BrandId = beer.BrandId
+            };
+
+            return Ok(beerDto);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult> Delete(int id)
+        {
+            var beer = await _context.Beers.FindAsync(id);
+            if (beer == null)
+            {
+                return NotFound();
+            }
+
+            _context.Beers.Remove(beer);
+            await _context.SaveChangesAsync();
+
+            return Ok();
+        }
     }
 }
